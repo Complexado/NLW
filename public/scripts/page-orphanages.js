1f8f443fc8175beeffa488ff4acba1876601bf4f
GuilhermeAlexanderGIT <guilhermealexander23@gmail.com>
2020-12-08 13:34:06 -0300

    att map

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,7 +1,27 @@
-const map = L.map('mapid').setView([-5.521783, -47.498197], 13);
+//create map
+const map = L.map('mapid').setView([-5.511010, -47.471636],15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
- {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}
-).addTo(map);
+
+//create and add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize:[58,68],
+    iconAnchor: [29,68],
+    popupAnchor:[170,2]
+})
+
+//create popup orverlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeigth: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+//create and add marker
+L.marker([-5.511010, -47.471636],{icon})
+.addTo(map)
+.bindPopup(popup)
